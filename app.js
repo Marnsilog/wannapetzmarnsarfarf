e46b7e5c29776122ece0219710ae69038dd6fd7f
Marniel Corsiga <corsiga.242722@globalcity.sti.edu.ph>
2024-09-22 17:52:39 +0800

    DWADWAD

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ const app = express();
 // Database connection
 const db = mysql.createConnection({
     host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
+    //port: process.env.DB_PORT,
     user: process.env.DB_USER,
     password: process.env.DB_PASS,
     database: process.env.DB_NAME
